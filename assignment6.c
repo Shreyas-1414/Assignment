a4da37cb4f99d3c396864413092eaa65ea39b31b
Shreyas-1414 <shreyasbhat78@gmail.com>
2026-04-23 10:14:23 +0530

    Refactor factorial calculation in assignment6.c

--- assignment6.c
+++ assignment6.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void main() {
 
-    // number and result variable
+    
     int n, factorial;
     printf("Enter the number: ");
-    scanf("%d", &n); //Take in number from user
+    scanf("%d", &n); 
     factorial = 1; 
 
 
-    while (n > 0) //Looping multiplication until n = 1
+    while (n > 0) 
     {
         factorial = factorial * n;
         n = n - 1;
